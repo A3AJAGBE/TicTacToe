4566907c167aabc0d0312afb930cfb6d58af9a84
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-23 06:30:46 +0000

    Add logo and player input

---
+++ main.py
@@ -0,0 +1,8 @@
+"""A Tic Tac Toe game application."""
+
+from logo import logo
+
+print(logo)
+print('Use the display numbers to play in your preferred position\n')
+player_1 = int(input('What position? '))
+print(player_1)
