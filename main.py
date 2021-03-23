43f5fc0d3a76eef8075329f96a882cb2d19687a8
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-23 07:11:57 +0000

    Add game design function

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ from logo import logo
 
 print(logo)
 print('Use the display numbers to play in your preferred position\n')
-player_1 = int(input('What position? '))
-print(player_1)
+# player_1 = int(input('What position? '))
+# print(player_1)
+
+
+def game_design(position):
+    print("\n"
+          "         |         |         \n"
+          f"    {1}    |    {2}    |    {3}  \n"
+          "_________|_________|_________\n"
+          "         |         |         \n"
+          f"    {4}    |    {5}    |    {6}  \n"
+          "_________|_________|_________\n"
+          "         |         |         \n"
+          f"    {7}    |    {8}    |    {9}  \n"
+          "         |         |         \n"
+          "\n")
+
+
