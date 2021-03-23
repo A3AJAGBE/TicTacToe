34b51ff7dc414a56265a6aab41a1bab7c8194502
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-23 07:33:48 +0000

    Add player input validation

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ from logo import logo
 
 print(logo)
 print('Use the display numbers to play in your preferred position\n')
-# player_1 = int(input('What position? '))
-# print(player_1)
 
 
 def game_design(position):
     print("\n"
           "         |         |         \n"
-          f"    {1}    |    {2}    |    {3}  \n"
+          f"    {position[0]}    |    {position[1]}    |    {position[2]}  \n"
           "_________|_________|_________\n"
           "         |         |         \n"
-          f"    {4}    |    {5}    |    {6}  \n"
+          f"    {position[3]}    |    {position[4]}    |    {position[5]}  \n"
           "_________|_________|_________\n"
           "         |         |         \n"
-          f"    {7}    |    {8}    |    {9}  \n"
+          f"    {position[6]}    |    {position[7]}    |    {position[8]}  \n"
           "         |         |         \n"
           "\n")
 
 
+game_start = True
+while game_start:
+    player_1 = input('What position? ')
+
+    if player_1.isdigit():
+        if int(player_1) < 1 or int(player_1) > 9:
+            print('Invalid input, must be between 1 and 9.\n')
+        else:
+            print(player_1)
+            game_start = False
+    else:
+        print('Input must be an integer.\n')
