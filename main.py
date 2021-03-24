50d5baa1cb0937dc043f098dc3ae2da85a763b5c
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-24 15:31:44 +0000

    Add game function

--- main.py
+++ main.py
@@ -9,26 +9,41 @@ print('Use the display numbers to play in your preferred position\n')
 def game_design(position):
     print("\n"
           "         |         |         \n"
-          f"    {position[0]}    |    {position[1]}    |    {position[2]}  \n"
+          f"    {position[1]}    |    {position[2]}    |    {position[3]}  \n"
           "_________|_________|_________\n"
           "         |         |         \n"
-          f"    {position[3]}    |    {position[4]}    |    {position[5]}  \n"
+          f"    {position[4]}    |    {position[5]}    |    {position[6]}  \n"
           "_________|_________|_________\n"
           "         |         |         \n"
-          f"    {position[6]}    |    {position[7]}    |    {position[8]}  \n"
+          f"    {position[7]}    |    {position[8]}    |    {position[9]}  \n"
           "         |         |         \n"
           "\n")
 
 
-game_start = True
-while game_start:
-    player_1 = input('What position? ')
+def game():
+    current_player = 'X'
 
-    if player_1.isdigit():
-        if int(player_1) < 1 or int(player_1) > 9:
+    gamePositions = [' ' for x in range(10)]
+
+    playerPositions = {'X': [], 'O': []}
+
+    game_start = True
+    while game_start:
+
+        try:
+            game_move = int(input('What position is your game move? '))
+        except ValueError:
+            print('Input must be an integer.\n')
+
+        if game_move < 1 or game_move > 9:
             print('Invalid input, must be between 1 and 9.\n')
+        elif gamePositions[game_move] != ' ':
+            print('Position occupied, Play again\n')
         else:
-            print(player_1)
-            game_start = False
-    else:
-        print('Input must be an integer.\n')
+            gamePositions[game_move] = current_player
+            playerPositions[current_player].append(game_move)
+
+        game_design(gamePositions)
+
+
+game()
